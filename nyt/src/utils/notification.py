e406f99d40953521b42ea10358eb839385d332d9
ramsy0dev <ramsy0dev@gmail.com>
2024-05-30 18:13:09 +0100

    update(notification): use pre-determined path for the logo

--- nyt/src/utils/notification.py
+++ nyt/src/utils/notification.py
@@ -1,11 +1,9 @@
-import os
 import notifypy
 import functools
 
 # NOTE: This is a temporary fix for the UnsupportedPlatform exception being raised
 # by notifypy because we are using Python 3.12.
 # PR: https://github.com/ms7m/notify-py/pull/55
-
 notifypy.Notify._selected_notification_system = functools.partial(notifypy.Notify._selected_notification_system, override_windows_version_detection=True)
 
 from nyt import constant
@@ -21,20 +19,11 @@ def send_notification(summary_text: str, message: str) -> None:
     Returns:
         None.
     """
-    to_replace = "\\nyt\\src\\utils" if constant.PLATFORM == "win32" else "/nyt/src/utils"
-    script_dir_name = os.path.dirname(__file__).replace(to_replace, "")
-
     notify = notifypy.Notify()
     
     notify.application_name = constant.PACKAGE
     notify.title = summary_text
     notify.message = message
-
-    if constant.PLATFORM == "win32":
-        icon_path = script_dir_name + "\\" + constant.NYT_HIGH_RESOLUTION_LOGO.replace("/", "\\")
-    else:
-        icon_path = script_dir_name + "/" + constant.NYT_HIGH_RESOLUTION_LOGO
-
-    notify.icon = icon_path
-
+    notify.icon = constant.NYT_HIGH_RESOLUTION_LOGO
+    
     notify.send()
