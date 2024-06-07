451b532d0fb644398451b5a1ac356bedf98e1265
ramsy0dev <ramsy0dev@gmail.com>
2024-06-07 06:40:03 +0100

    Removed reflex UI because it sucks

--- nyt-ui/nyt_ui/state.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-
-class State(rx.State):
-    """Define empty state to allow access to rx.State.router."""
-    
-    @rx.var
-    def video_id(self) -> str:
-        return self.router.page.params.get("video_id", None)
